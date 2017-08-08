5ae9fbeda73d4193372416a8e82ab52648512f09
Fabian Thommen <fabtho@users.noreply.github.com>
2017-08-08 11:23:15 +0200

    also allow installation in TYPO3 Version 6.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.2',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-8.99.99',
+			'typo3' => '6.2.0-8.99.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
